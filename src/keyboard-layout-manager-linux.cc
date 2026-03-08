935f86708c19b25058375a06a425555fc06dcc80
Andrew Dupont <github@andrewdupont.net>
2026-03-07 23:18:37 -0800

    Debugging

--- src/keyboard-layout-manager-linux.cc
+++ src/keyboard-layout-manager-linux.cc
@@ -522,6 +522,16 @@ static uint FindAltGrMask(Display *display) {
   }
 
   XkbFreeKeyboard(xkb, XkbAllComponentsMask, True);
+  // Diagnostic: print how many level-2 entries exist at all
+  int totalLevel2 = 0;
+  for (int t = 0; t < xkb->map->num_types; t++) {
+    XkbKeyTypePtr keyType = &xkb->map->types[t];
+    for (int i = 0; i < keyType->map_count; i++) {
+      if (keyType->map[i].active && keyType->map[i].level == 2)
+        totalLevel2++;
+    }
+  }
+  printf("num_types: %d, total level-2 entries: %d\n", xkb->map->num_types, totalLevel2);
   return 0;
 }
 
